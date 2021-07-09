2f41475db138c9bf2e05230b6b87e5374414704e
unknown <vimit.athawale02@gmail.com>
2021-07-09 13:24:35 +0530

    kyc wise data handled on both dist and users pages

--- src/config/query.js
+++ src/config/query.js
@@ -3,5 +3,6 @@ export const QUERY_STALE_TIME = 3 * 60 * 1000
 // Keep these properties sorted alphabetically for easier scanning.
 export const QueryKeys = {
   ALL_USERS: 'all_users',
+  ALL_USERS_BY_KYC:'all_users_by_kyc',
   USER_DETAILS:"user_details"
 }
--- src/services/api/UserService.js
+++ src/services/api/UserService.js
@@ -3,21 +3,19 @@ import { translateUser } from "./translators/translateUser";
 
 async function getAll(qParams = {}) {
   const url = `/admin/getAllUsers`;
-  // const queryFn = (queryParams) => {
   const params = {
-    // sort: 'name:asc',
     ...qParams,
-    // ...queryParams,
   };
+  const res = apiClient.get(url, { params });
+  return res;
+}
 
-  // const queryFn = (queryParams) => {
-  //   const params = {
-  //     ...qParams,
-  //     ...queryParams,
-  //   };
-  //   return apiClient.get(url, { params });
-  // };
-  // const res = await getAll({ queryFn });
+async function getAllByKyc(qParams = {}, kycFlag) {
+  const url = `/admin/getAllUsersByKyc/${kycFlag}`;
+  const params = {
+    limit: qParams.limit,
+    page: qParams.page,
+  };
   const res = apiClient.get(url, { params });
   return res;
 }
@@ -48,6 +46,7 @@ async function update(userId, body) {
 
 export default {
   getAll,
+  getAllByKyc,
   getById,
   create,
   update,
